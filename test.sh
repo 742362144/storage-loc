013fa9ef6ff6a65a7007e64d71535c7c29b211a0
unknown <742362144@qq.com>
2020-11-03 15:54:18 +0800

    add benchmark

--- test.sh
+++
@@ -1,2 +0,0 @@
-./benchmark -host 133.133.135.22 -port 50051 -model 1 -parallel 32 -size 16 -num 10000 >> result
-./benchmark -host 133.133.135.22 -port 50051 -model 2 -parallel 32 -size 16 -num 10000 >> result
